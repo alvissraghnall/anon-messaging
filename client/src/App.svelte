2a86d01a4701ee0860fe132659cc321b1c472c9d
Alviss Raghnall <isiomastewart@gmail.com>
2022-07-20 20:30:09 +0100

    Omah Lay - Woman

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import { Router } from 'svelte-router-spa';
-  import { routes } from './routes'
+  import { routes } from './routes';
+  import "./tailwind.css";
 </script>
 
 <Router {routes} />
