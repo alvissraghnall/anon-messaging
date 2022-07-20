b172e0f7ac4d5fb28f7daf41b7d7c7cf745bbd8f
Alviss Raghnall <isiomastewart@gmail.com>
2022-07-20 15:02:25 +0100

    Earl Sweatshirt - Red Water

--- client/src/pages/Login.svelte
+++ client/src/pages/Login.svelte
@@ -1,19 +1,66 @@
 <style>
   import FormView from "../views/Form.view";
   import { Navigate } from "svelte-router-spa";
+  import { createForm } from "svelte-forms-lib";
+    import * as yup from "yup";
+
+    const { form, errors, state, handleChange, handleSubmit } = createForm({
+      initialValues: {
+        password: "",
+        email: ""
+      },
+      validationSchema: yup.object().shape({
+        email: yup
+          .string()
+          .email()
+          .required(),
+        password: yup.string()
+          .required()
+      }),
+      onSubmit: values => {
+        alert(JSON.stringify(values));
+      }
+    });
 </style>
 
 <div id="login">
   <FormView>
-    <form class="space-y-6" action="#">
+    <form class="space-y-6" on:submit={handleSubmit}>
       <h5 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
       <div>
         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required="">
+        <input 
+          type="email" 
+          name="email" 
+          id="email" 
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" 
+          placeholder="name@company.com" 
+          required=""
+          on:change={handleChange}
+          on:blur={handleChange}
+          bind:value={$form.email}
+        >
+        {#if $errors.email}
+          <small>{$errors.email}</small>
+        {/if}
       </div>
       <div>
         <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
-        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="">
+        <input 
+          type="password" 
+          name="password" 
+          id="password" 
+          placeholder="••••••••" 
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" 
+          required=""
+          on:change={handleChange}
+          on:blur={handleChange}
+          bind:value={$form.password}
+        >
+        {#if $errors.password}
+          <small>{$errors.password}</small>
+        {/if}
+
       </div>
       <div class="flex items-start">
         <div class="flex items-start">
@@ -22,7 +69,7 @@
           </div>
           <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
         </div>
-        <a href="#" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+        <Navigate to="password/forgot" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</Navigate>
       </div>
       <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
       <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
@@ -33,5 +80,10 @@
 </div>
 
 <style>
-  
+  small {
+    display: block;
+    font-size: 12px;
+    color: #ff6b6b;
+    margin-top: 10px;
+  }
 </style>
--- client/src/views/Form.view.svelte
+++ client/src/views/Form.view.svelte
@@ -5,6 +5,9 @@
 
 <div class="wrap-body">
   <FormCard>
+    <div class="page-logo">
+      <img src="assets/logo-icon.png" alt="Piree Logo">
+    </div>
     <slot></slot>
   </FormCard>
 </div>
@@ -13,5 +16,20 @@
 <style>
   .wrap-body {
     background-image: linear-gradient( 135deg, #92FFC0 10%, #002661 100%);
+  }
+  
+  .page-logo {
+    display: block;
+    font-family: Poppins-Bold;
+    font-size: 39px;
+    color: #fff;
+    line-height: 1.2;
+    text-align: center;
+  }
+  
+  img {
+    padding-bottom: 45px;
+    vertical-align: middle;
+    border-style: none;
   }
 </style>
