f65b6d0cee891077f38bbc4de19a5fa4c9a64ec0
Alviss Raghnall <isiomastewart@gmail.com>
2022-07-22 23:49:22 +0100

    Don Toliver - After Party

---
+++ client/src/util/advantages.js
@@ -0,0 +1,17 @@
+export const advantages = [
+  {
+    header: "Anonymity",
+    text: "Our Platform ensures your privacy so that you stay anonymous everytime you send someone a secret message. You are anonymous until you ever choose to reveal your identity."
+  }, {
+    header: "Safe & secure",
+    text: "Safety of our users using this anonymous messaging platform is very important for us. We have got reporting systems so that you can report anything that you do not like to see."
+  }, 
+  {
+    header: "24/7 support",
+    text: "If there is anything that you need help with related to our anonymous messaging and feedback platform, We are always here for you. 24 hours a day and 7 days a week."
+  },
+  {
+    header: "Easy to use",
+    text: "We are constantly working on Piree as a platform to make it as user friendly as possible. As of now you could get started by filling in your details in the registration form."
+  }
+]
