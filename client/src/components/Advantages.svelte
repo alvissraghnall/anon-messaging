9df030bee434b9585b072dd0fa28a8309b7d2762
alvissraghnall <isiomastewart@gmail.com>
2022-07-23 02:38:07 +0100

    Fireboy DML - Friday feeling

---
+++ client/src/components/Advantages.svelte
@@ -0,0 +1,22 @@
+<section>
+    <div class="woo">
+        <h4>
+            Why use Piree?
+        </h4>
+        <p>
+            Our  Anonymous messaging app comes along with many great features. Here we are going to list some of them. Have a look below:
+        </p>
+    </div>
+</section>
+
+<style>
+    section {
+        width: 100%;
+        background-color: #fff;
+    }
+
+    .woo {
+        text-align: center;
+        padding: 8px;
+    }
+</style>
