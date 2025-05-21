4c95a515dea965922a3c7accf7a50bc657a14e78
Alviss Raghnall <isiomastewart@gmail.com>
2025-05-21 11:22:32 +0100

    removed irrelevant migrations and drop table ...

--- microservice/db/migrations/20250212231155_next_schema.sql
+++
@@ -1,9 +0,0 @@
--- Add migration script here
-CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id TEXT UNIQUE NOT NULL,
-        public_key_hash TEXT NOT NULL,
-        encrypted_private_key TEXT NOT NULL,
-        encryption_salt TEXT NOT NULL,
-        encryption_nonce TEXT NOT NULL
-);
